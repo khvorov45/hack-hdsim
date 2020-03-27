4990a93348760f1c169dcb722bce11569d74ec5c
Arseniy Khvorov <khvorov45@gmail.com>
2020-03-27 22:52:36 +1100

    added a 2-bit And gate

--- hack-hdsim-cli/src/main.rs
+++ hack-hdsim-cli/src/main.rs
@@ -1,3 +1,13 @@
+extern crate hack_hdsim_lib;
+
+use hack_hdsim_lib::And2;
+
 fn main() {
-    println!("Hello, world!");
+    let and2 = And2::new(true, false);
+    println!(
+        "Created And2 gate, a: {}, b: {}, out: {}",
+        and2.in_a(),
+        and2.in_b(),
+        and2.out()
+    )
 }
