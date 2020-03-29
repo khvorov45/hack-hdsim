4447e945da2cb36cb0171c97303fec5ea293ca3e
Arseniy Khvorov <khvorov45@gmail.com>
2020-03-30 07:36:21 +1100

    basic printing of string parts

--- hack-hdsim-lib/src/tokeniser.rs
+++ hack-hdsim-lib/src/tokeniser.rs
@@ -1,3 +1,8 @@
 pub fn tokenise_hdl(contents: String) {
     println!("String to tokenise:\n{}", contents);
+    let temp = contents.split(' ');
+    println!("Tokens:\n");
+    for tok in temp {
+        println!("{}", tok);
+    }
 }
