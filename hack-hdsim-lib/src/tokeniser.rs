1d53e5b584fd27fdce58b05292a731c998b1b2e8
Arseniy Khvorov <khvorov45@gmail.com>
2020-03-31 07:56:44 +1100

    added the beginnings of tokenising

--- hack-hdsim-lib/src/tokeniser.rs
+++ hack-hdsim-lib/src/tokeniser.rs
@@ -1,8 +1,34 @@
+const _KEYWORDS: &[&str] = &["CHIP", "IN", "OUT", "PARTS"];
+
+#[derive(Debug)]
+struct Token {
+    literal: String,
+    token_type: TokenType,
+}
+
+#[derive(Debug)]
+enum TokenType {
+    Keyword,
+    Unknown,
+}
+
 pub fn tokenise_hdl(contents: String) {
     println!("String to tokenise:\n{}", contents);
+    // Probably need to remove comments here
     let temp = contents.split(' ');
     println!("Tokens:\n");
+    let mut tokens = Vec::new();
     for tok in temp {
-        println!("{}", tok);
+        let literal = tok.trim();
+        let token_type = match literal {
+            "CHIP" => TokenType::Keyword,
+            _ => TokenType::Unknown,
+        };
+        let token = Token {
+            literal: String::from(literal),
+            token_type,
+        };
+        println!("{:?}", token);
+        tokens.push(token);
     }
 }
