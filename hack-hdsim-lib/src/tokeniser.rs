1e1c5afbd1872be72929e3fdd7e3a8f998976b08
Arseniy Khvorov <khvorov45@gmail.com>
2020-04-02 07:50:04 +1100

    function to skip whitespaces

--- hack-hdsim-lib/src/tokeniser.rs
+++ hack-hdsim-lib/src/tokeniser.rs
@@ -30,20 +30,30 @@ impl TokenStream {
 }
 
 struct Tokeniser<'a> {
-    itr: std::str::Chars<'a>,
+    itr: std::iter::Peekable<std::str::Chars<'a>>,
 }
 
 impl<'a> Tokeniser<'a> {
     fn new(contents: &'a str) -> Self {
         Self {
-            itr: contents.chars(),
+            itr: contents.chars().peekable(),
         }
     }
     fn tokenise_chip(&mut self) -> Vec<Token> {
         let tokens = Vec::new();
         while let Some(ch) = self.itr.next() {
+            self.skip_nontokens();
             print!("{:?} ", ch);
         }
         tokens
+    }
+    fn skip_nontokens(&mut self) {
+        while let Some(ch) = self.itr.peek() {
+            if ch.is_whitespace() {
+                self.itr.next();
+                continue;
+            }
+            break;
+        }
     }
 }
