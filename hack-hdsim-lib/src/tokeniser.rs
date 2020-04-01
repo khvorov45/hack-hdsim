85362082682b69f2b2657551068f913ca8b19dd0
Arseniy Khvorov <khvorov45@gmail.com>
2020-04-02 07:42:12 +1100

    added the tokeniser struct

--- hack-hdsim-lib/src/tokeniser.rs
+++ hack-hdsim-lib/src/tokeniser.rs
@@ -20,11 +20,30 @@ pub struct TokenStream {
 
 impl TokenStream {
     pub fn new(contents: &str) -> Self {
-        println!("Contents:\n{}", contents);
-        let tokens = Vec::new();
+        let mut tokeniser = Tokeniser::new(contents);
+        let tokens = tokeniser.tokenise_chip();
         Self { tokens }
     }
     pub fn tokens(&self) -> &Vec<Token> {
         &self.tokens
+    }
+}
+
+struct Tokeniser<'a> {
+    itr: std::str::Chars<'a>,
+}
+
+impl<'a> Tokeniser<'a> {
+    fn new(contents: &'a str) -> Self {
+        Self {
+            itr: contents.chars(),
+        }
+    }
+    fn tokenise_chip(&mut self) -> Vec<Token> {
+        let tokens = Vec::new();
+        while let Some(ch) = self.itr.next() {
+            print!("{:?} ", ch);
+        }
+        tokens
     }
 }
