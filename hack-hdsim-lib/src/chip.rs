19d6f87e47fa5d4ee4b3b3e60135fc028a35658a
Arseniy Khvorov <khvorov45@gmail.com>
2020-04-13 09:24:23 +1000

    added the iterface type to chip

--- hack-hdsim-lib/src/chip.rs
+++ hack-hdsim-lib/src/chip.rs
@@ -1,14 +1,31 @@
 pub type Pin = bool;
 
-pub type PinLine = Vec<Pin>;
+pub type Pinline = Vec<Pin>;
 
-pub struct Chip {
-    _input: Vec<PinLine>,
-    _output: Vec<PinLine>,
+pub type Interface = std::collections::HashMap<String, Pinline>;
+
+pub fn add_line(
+    interface: &mut Interface,
+    line_name: &str,
+    line_capacity: usize,
+) {
+    interface.insert(
+        String::from(line_name),
+        Pinline::with_capacity(line_capacity),
+    );
 }
 
-impl Chip {
-    pub fn new(_input: Vec<PinLine>, _output: Vec<PinLine>) -> Self {
-        Self { _input, _output }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn add_line_test() {
+        let mut input = Interface::new();
+        add_line(&mut input, "a", 1);
+        add_line(&mut input, "b", 16);
+        assert!(input.contains_key("a"));
+        assert!(input.contains_key("b"));
+        assert_eq!(input["a"].capacity(), 1);
+        assert_eq!(input["b"].capacity(), 16);
     }
 }
