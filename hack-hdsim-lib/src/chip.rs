6c2a2e71a5555802b1314615c43654f17a916b0a
Arseniy Khvorov <khvorov45@gmail.com>
2020-04-17 14:33:36 +1000

    removed process

--- hack-hdsim-lib/src/chip.rs
+++ hack-hdsim-lib/src/chip.rs
@@ -40,7 +40,7 @@ impl Interface {
     pub fn set(&mut self, name: &str, vals: Vec<Pin>) {
         for pinline in &mut self.pinlines {
             if pinline.name.as_str() == name {
-                pinline.pins = vals;
+                pinline.set(vals);
                 return;
             }
         }
@@ -57,25 +57,23 @@ impl Interface {
 }
 
 #[derive(Debug)]
-pub struct Chip<F: Fn(Interface, Interface) -> ()> {
+pub struct Chip {
     input: Interface,
     output: Interface,
-    process: F,
 }
 
-impl<F: Fn(Interface, Interface) -> ()> Chip<F> {
-    pub fn new(input: Interface, output: Interface, process: F) -> Self {
-        Self {
-            input,
-            output,
-            process,
-        }
+impl Chip {
+    pub fn new(input: Interface, output: Interface) -> Self {
+        Self { input, output }
     }
     pub fn input(&self) -> &Interface {
         &self.input
     }
     pub fn output(&self) -> &Interface {
         &self.output
+    }
+    fn _call() {
+        // this needs to be able to call other chips somehow
     }
 }
 
@@ -84,15 +82,14 @@ mod tests {
     use super::*;
     #[test]
     fn chip_new() {
-        let and_chip = Chip::new(
+        let mut and_chip = Chip::new(
             Interface::new(&[("a", 1), ("b", 1)]),
             Interface::new(&[("c", 1)]),
-            |input, mut output| {
-                output.set("c", vec![input.get("a")[1] && input.get("b")[1]]);
-            },
         );
         println!("{:#?}", and_chip.input);
         println!("{:#?}", and_chip.output);
+        and_chip.input.set("a", vec![true]);
+        and_chip.input.set("b", vec![true]);
         // assert!(false);
     }
 }
